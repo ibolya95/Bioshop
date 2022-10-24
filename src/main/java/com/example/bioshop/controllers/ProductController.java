141c347d2ba6c3965873e70f4635faca62fd7f04
ibolya95 <jurinka.ibi@gmail.com>
2022-10-24 16:08:45 +0200

    Add new item

--- src/main/java/com/example/bioshop/controllers/ProductController.java
+++ src/main/java/com/example/bioshop/controllers/ProductController.java
@@ -1,10 +1,12 @@
 package com.example.bioshop.controllers;
 
+import com.example.bioshop.models.Product;
 import com.example.bioshop.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,10 +20,20 @@ public class ProductController {
         this.productService = productService;
     }
 
-
     @GetMapping
     public String listProducts(Model model) {
         model.addAttribute("items", productService.products());
         return "index";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "create";
+    }
+
+    @PostMapping("/store")
+    public String store(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/bioshop";
     }
 }
